bfa343f0bad0535b111003208ea3ad9808c6b172
Pulkit Goyal <7895pulkit@gmail.com>
2018-02-14 16:38:15 +0530

    py3: add couple of missing b'' in fakemergerecord.py
    # skip-blame as we are just adding b'' prefixes
    
    Differential Revision: https://phab.mercurial-scm.org/D2262

--- tests/fakemergerecord.py
+++ tests/fakemergerecord.py
@@ -19,8 +19,8 @@ def fakemergerecord(ui, repo, *pats, **opts):
     with repo.wlock():
         ms = merge.mergestate.read(repo)
         records = ms._makerecords()
-        if opts.get('mandatory'):
+        if opts.get(b'mandatory'):
             records.append((b'X', b'mandatory record'))
-        if opts.get('advisory'):
+        if opts.get(b'advisory'):
             records.append((b'x', b'advisory record'))
         ms._writerecords(records)

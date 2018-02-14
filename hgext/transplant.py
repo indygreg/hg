44b09b2730498d0b76b18b316aac3545f2c7293e
Pulkit Goyal <7895pulkit@gmail.com>
2018-02-14 17:41:27 +0530

    py3: replace file() with open() in transplant.py
    Differential Revision: https://phab.mercurial-scm.org/D2267

--- hgext/transplant.py
+++ hgext/transplant.py
@@ -274,7 +274,7 @@ class transplanter(object):
                                     },
                            onerr=error.Abort, errprefix=_('filter failed'),
                            blockedtag='transplant_filter')
-            user, date, msg = self.parselog(file(headerfile))[1:4]
+            user, date, msg = self.parselog(open(headerfile, 'rb'))[1:4]
         finally:
             os.unlink(headerfile)
 

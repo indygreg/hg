d454bc175c1d38f8a923ccc2443da408de18aba2
Augie Fackler <augie@google.com>
2018-03-04 11:49:33 -0500

    util: also silence py3 warnings from codec module
    Fixes warnings like this:
    
    +  mercurial/util.py:2446: DeprecationWarning: invalid escape sequence '\d'
    +    return codecs.escape_decode(s)[0]
    
    Differential Revision: https://phab.mercurial-scm.org/D2642

--- mercurial/util.py
+++ mercurial/util.py
@@ -249,6 +249,8 @@ if _dowarn and pycompat.ispy3:
     # silence warning emitted by passing user string to re.sub()
     warnings.filterwarnings(r'ignore', r'bad escape', DeprecationWarning,
                             r'mercurial')
+    warnings.filterwarnings(r'ignore', r'invalid escape sequence',
+                            DeprecationWarning, r'mercurial')
 
 def nouideprecwarn(msg, version, stacklevel=1):
     """Issue an python native deprecation warning

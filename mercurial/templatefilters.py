61120895fbc84e95d346289733f0bd3193f8bdd8
Yuya Nishihara <yuya@tcha.org>
2018-03-24 18:03:23 +0900

    templatefilters: deprecate hgdate as {date|hgdate} is the default format

--- mercurial/templatefilters.py
+++ mercurial/templatefilters.py
@@ -241,7 +241,7 @@ def hexfilter(text):
 @templatefilter('hgdate', intype=templateutil.date)
 def hgdate(text):
     """Date. Returns the date as a pair of numbers: "1157407993
-    25200" (Unix timestamp, timezone offset).
+    25200" (Unix timestamp, timezone offset). (DEPRECATED)
     """
     return "%d %d" % text
 

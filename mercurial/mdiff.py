ca626213d50a6965313e7fdfb01b1aaf057caa41
Pulkit Goyal <7895pulkit@gmail.com>
2018-04-05 17:29:32 +0530

    py3: add missing b'' prefix in mdiff.py
    Transformer won't add b'' prefix because that value is already have a r''
    prefix.
    
    # skip-blame because just b'' prefix
    
    Differential Revision: https://phab.mercurial-scm.org/D3132

--- mercurial/mdiff.py
+++ mercurial/mdiff.py
@@ -97,7 +97,7 @@ def wsclean(opts, text, blank=True):
     if blank and opts.ignoreblanklines:
         text = re.sub('\n+', '\n', text).strip('\n')
     if opts.ignorewseol:
-        text = re.sub(br'[ \t\r\f]+\n', r'\n', text)
+        text = re.sub(br'[ \t\r\f]+\n', br'\n', text)
     return text
 
 def splitblock(base1, lines1, base2, lines2, opts):

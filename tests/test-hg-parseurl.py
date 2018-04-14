d93493981b8d55b9a1a4fe4c3e83a7ee08554b96
Augie Fackler <augie@google.com>
2018-04-14 11:04:58 -0400

    tests: port test-hg-parseurl.py to unittest
    Differential Revision: https://phab.mercurial-scm.org/D3373

--- tests/test-hg-parseurl.py
+++ tests/test-hg-parseurl.py
@@ -1,17 +1,34 @@
 from __future__ import absolute_import, print_function
 
+import unittest
+
 from mercurial import (
     hg,
 )
 
-def testparse(url, branch=[]):
-    print('%s, branches: %r' % hg.parseurl(url, branch))
+class ParseRequestTests(unittest.TestCase):
+    def testparse(self):
 
-testparse('http://example.com/no/anchor')
-testparse('http://example.com/an/anchor#foo')
-testparse('http://example.com/no/anchor/branches', branch=['foo'])
-testparse('http://example.com/an/anchor/branches#bar', branch=['foo'])
-testparse('http://example.com/an/anchor/branches-None#foo', branch=None)
-testparse('http://example.com/')
-testparse('http://example.com')
-testparse('http://example.com#foo')
+        self.assertEqual(hg.parseurl('http://example.com/no/anchor'),
+                         ('http://example.com/no/anchor', (None, [])))
+        self.assertEqual(hg.parseurl('http://example.com/an/anchor#foo'),
+                         ('http://example.com/an/anchor', ('foo', [])))
+        self.assertEqual(
+            hg.parseurl('http://example.com/no/anchor/branches', ['foo']),
+            ('http://example.com/no/anchor/branches', (None, ['foo'])))
+        self.assertEqual(
+            hg.parseurl('http://example.com/an/anchor/branches#bar', ['foo']),
+            ('http://example.com/an/anchor/branches', ('bar', ['foo'])))
+        self.assertEqual(hg.parseurl(
+            'http://example.com/an/anchor/branches-None#foo', None),
+            ('http://example.com/an/anchor/branches-None', ('foo', [])))
+        self.assertEqual(hg.parseurl('http://example.com/'),
+                         ('http://example.com/', (None, [])))
+        self.assertEqual(hg.parseurl('http://example.com'),
+                         ('http://example.com/', (None, [])))
+        self.assertEqual(hg.parseurl('http://example.com#foo'),
+                         ('http://example.com/', ('foo', [])))
+
+if __name__ == '__main__':
+    import silenttestrunner
+    silenttestrunner.main(__name__)

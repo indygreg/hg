b5c95c527f396a11d350cf06c441ee255a3d4ae6
Augie Fackler <augie@google.com>
2018-04-27 00:17:17 -0400

    tests: port test-walkrepo.py to Python 3
    # skip-blame just b prefixes, nothing interesting
    
    Differential Revision: https://phab.mercurial-scm.org/D3473

--- tests/test-walkrepo.py
+++ tests/test-walkrepo.py
@@ -17,22 +17,22 @@ walkrepos = scmutil.walkrepos
 checklink = util.checklink
 
 u = uimod.ui.load()
-sym = checklink('.')
+sym = checklink(b'.')
 
-hg.repository(u, 'top1', create=1)
-mkdir('subdir')
-chdir('subdir')
-hg.repository(u, 'sub1', create=1)
-mkdir('subsubdir')
-chdir('subsubdir')
-hg.repository(u, 'subsub1', create=1)
+hg.repository(u, b'top1', create=1)
+mkdir(b'subdir')
+chdir(b'subdir')
+hg.repository(u, b'sub1', create=1)
+mkdir(b'subsubdir')
+chdir(b'subsubdir')
+hg.repository(u, b'subsub1', create=1)
 chdir(os.path.pardir)
 if sym:
-    os.symlink(os.path.pardir, 'circle')
-    os.symlink(pjoin('subsubdir', 'subsub1'), 'subsub1')
+    os.symlink(os.path.pardir, b'circle')
+    os.symlink(pjoin(b'subsubdir', b'subsub1'), b'subsub1')
 
 def runtest():
-    reposet = frozenset(walkrepos('.', followsym=True))
+    reposet = frozenset(walkrepos(b'.', followsym=True))
     if sym and (len(reposet) != 3):
         print("reposet = %r" % (reposet,))
         print(("Found %d repositories when I should have found 3"
@@ -41,19 +41,19 @@ def runtest():
         print("reposet = %r" % (reposet,))
         print(("Found %d repositories when I should have found 2"
                % (len(reposet),)))
-    sub1set = frozenset((pjoin('.', 'sub1'),
-                         pjoin('.', 'circle', 'subdir', 'sub1')))
+    sub1set = frozenset((pjoin(b'.', b'sub1'),
+                         pjoin(b'.', b'circle', b'subdir', b'sub1')))
     if len(sub1set & reposet) != 1:
         print("sub1set = %r" % (sub1set,))
         print("reposet = %r" % (reposet,))
         print("sub1set and reposet should have exactly one path in common.")
-    sub2set = frozenset((pjoin('.', 'subsub1'),
-                         pjoin('.', 'subsubdir', 'subsub1')))
+    sub2set = frozenset((pjoin(b'.', b'subsub1'),
+                         pjoin(b'.', b'subsubdir', b'subsub1')))
     if len(sub2set & reposet) != 1:
         print("sub2set = %r" % (sub2set,))
         print("reposet = %r" % (reposet,))
         print("sub2set and reposet should have exactly one path in common.")
-    sub3 = pjoin('.', 'circle', 'top1')
+    sub3 = pjoin(b'.', b'circle', b'top1')
     if sym and sub3 not in reposet:
         print("reposet = %r" % (reposet,))
         print("Symbolic links are supported and %s is not in reposet" % (sub3,))

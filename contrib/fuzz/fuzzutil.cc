14d0432699aebbd77fb68ee0198a51fee7b77459
Yuya Nishihara <yuya@tcha.org>
2018-06-03 13:12:32 +0900

    fuzz: fix use of undeclared function memcpy()

--- contrib/fuzz/fuzzutil.cc
+++ contrib/fuzz/fuzzutil.cc
@@ -1,5 +1,6 @@
 #include "fuzzutil.h"
 
+#include <cstring>
 #include <utility>
 
 contrib::optional<two_inputs> SplitInputs(const uint8_t *Data, size_t Size)
@@ -13,11 +14,11 @@ contrib::optional<two_inputs> SplitInputs(const uint8_t *Data, size_t Size)
 	// Copy inputs to new allocations so if bdiff over-reads
 	// AddressSanitizer can detect it.
 	std::unique_ptr<char[]> left(new char[left_size]);
-	memcpy(left.get(), Data + 1, left_size);
+	std::memcpy(left.get(), Data + 1, left_size);
 	// right starts at the next byte after left ends
 	size_t right_size = Size - (left_size + 1);
 	std::unique_ptr<char[]> right(new char[right_size]);
-	memcpy(right.get(), Data + 1 + left_size, right_size);
+	std::memcpy(right.get(), Data + 1 + left_size, right_size);
 	LOG(2) << "inputs are  " << left_size << " and " << right_size
 	       << " bytes" << std::endl;
 	two_inputs result = {std::move(right), right_size, std::move(left),

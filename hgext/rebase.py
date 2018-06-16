69379211b7b4c50cd0e6b3b0d0a61d0579817b39
Sushil khanchi <sushilkhanchi97@gmail.com>
2018-06-16 18:36:25 +0530

    rebase: delete the comment which was not following "do not eat my data"
    Differential Revision: https://phab.mercurial-scm.org/D3756

--- hgext/rebase.py
+++ hgext/rebase.py
@@ -1588,7 +1588,6 @@ def abort(repo, originalwd, destmap, state, activebookmark=None):
 
             # Strip from the first rebased revision
             if rebased:
-                # no backup of rebased cset versions needed
                 repair.strip(repo.ui, repo, strippoints)
 
         if activebookmark and activebookmark in repo._bookmarks:

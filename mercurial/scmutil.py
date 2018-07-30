fc2f91de636b9cd50e28850de9a3a7cf0f919b45
Boris Feld <boris.feld@octobus.net>
2018-07-30 15:53:47 +0200

    sparse-revlog: also use sparse-revlog config as a general delta trigger
    Sparse revlog rely on general delta, so we should make sure it is used.

--- mercurial/scmutil.py
+++ mercurial/scmutil.py
@@ -1366,7 +1366,8 @@ def gdinitconfig(ui):
     """
     # experimental config: format.generaldelta
     return (ui.configbool('format', 'generaldelta')
-            or ui.configbool('format', 'usegeneraldelta'))
+            or ui.configbool('format', 'usegeneraldelta')
+            or ui.configbool('format', 'sparse-revlog'))
 
 def gddeltaconfig(ui):
     """helper function to know if incoming delta should be optimised

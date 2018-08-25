3bfcf307dec69a1a42c8d3fc2117973bfce9c2b1
Boris Feld <boris.feld@octobus.net>
2018-08-25 11:20:13 +0200

    phases: simplify revset in updatephases
    (Pointed by Yuya Nishihara)

--- mercurial/phases.py
+++ mercurial/phases.py
@@ -599,8 +599,8 @@ def updatephases(repo, trgetter, headsbyphase):
     # to update. This avoid creating empty transaction during no-op operation.
 
     for phase in allphases[:-1]:
-        revset = '%%ln - _phase(%d)' % phase
-        heads = [c.node() for c in repo.set(revset, headsbyphase[phase])]
+        revset = '%ln - _phase(%s)'
+        heads = [c.node() for c in repo.set(revset, headsbyphase[phase], phase)]
         if heads:
             advanceboundary(repo, trgetter(), phase, heads)
 

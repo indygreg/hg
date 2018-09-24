1ce62fd634b09953b4b50f6aa55b870ccb3bf5db
Yuya Nishihara <yuya@tcha.org>
2018-09-24 15:54:18 +0900

    rust-chg: add project skeleton
    This directory will host the reimplementation of cHg in Rust. It will use
    Tokio [1], and tokio-hglib [2] which I wrote for an oxidized CHg, no idea
    if there's such carbon bonding in nature btw.
    
     [1]: https://tokio.rs/
     [2]: https://bitbucket.org/yuja/tokio-hglib/
    
    The reasoning for depending on Tokio is that it will allow us to handle Unix
    signals in a safer way. Well, I believed that until I found a weird function,
    handlestopsignal(), in cHg codebase. It resends the same signal to the same
    process by temporarily masking the handler, which can't be inherently async.
    So the signal handlers will stay in C, which means there isn't actually much
    reason to write async code right now, other than I've already done most of
    the async stuff, and slightly easier pager handling.
    
    The reasoning for the rewrite is that it will eventually be possible to port
    server-side config validation back to the client side, which will reduce
    the complexity of the current daemon management. It will also encourage us
    to write frontend library (e.g. command line and config parsers) in Rust.
    
    The license is GPL2+ because it's likely to include derived work from the
    cHg of C. The rust/chg crate is excluded from the root workspace as it's
    unclear how the whole rust packages should be laid out. That can be revisited
    later.

---
+++ rust/chg/src/main.rs
@@ -0,0 +1,7 @@
+// Copyright 2018 Yuya Nishihara <yuya@tcha.org>
+//
+// This software may be used and distributed according to the terms of the
+// GNU General Public License version 2 or any later version.
+
+fn main() {
+}

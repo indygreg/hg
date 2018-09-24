e44b2d38d8ae6158f175c908ad68457b7a76251e
Yuya Nishihara <yuya@tcha.org>
2018-09-24 16:33:24 +0900

    rust-chg: add parser for request messages sent to "S" channel
    The data structure is documented at chgserver.py:channeledsystem().

--- rust/chg/src/lib.rs
+++ rust/chg/src/lib.rs
@@ -3,6 +3,9 @@
 // This software may be used and distributed according to the terms of the
 // GNU General Public License version 2 or any later version.
 
+extern crate bytes;
 extern crate libc;
+extern crate tokio_hglib;
 
+pub mod message;
 pub mod procutil;

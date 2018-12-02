6381eb1612123f5cd50782f3bf165e2998ec20be
Matt Harbison <matt_harbison@yahoo.com>
2018-12-01 21:42:48 -0500

    tests: apply binary mode to output in seq.py
    I noticed this when playing with running tests using WSL, and iterating over the
    output yielded '0\r', '1\r',...  Most of the other *.py tools do this, and `seq`
    on MSYS lacks '\r' in the output, so this is more consistent.

--- tests/seq.py
+++ tests/seq.py
@@ -8,7 +8,16 @@
 #   seq START STEP STOP   [START, STOP] stepping by STEP
 
 from __future__ import absolute_import, print_function
+import os
 import sys
+
+try:
+    import msvcrt
+    msvcrt.setmode(sys.stdin.fileno(), os.O_BINARY)
+    msvcrt.setmode(sys.stdout.fileno(), os.O_BINARY)
+    msvcrt.setmode(sys.stderr.fileno(), os.O_BINARY)
+except ImportError:
+    pass
 
 if sys.version_info[0] >= 3:
     xrange = range
